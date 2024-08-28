694b1bf1c56b0cf348193547e29273702d8d928d
AlexCampusNum <alex.chamoux@le-campus-numerique.fr>
2024-08-28 16:28:30 +0200

    début test

--- src/test/java/com/magasin/MagasinTest.java
+++ src/test/java/com/magasin/MagasinTest.java
@@ -7,11 +7,94 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class MagasinTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    void testRegularItem() {
+        Item[] items = new Item[] { new Item("Regular Item", 10, 20) };
         Magasin app = new Magasin(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals(9, items[0].sellIn);
+        assertEquals(19, items[0].quality);
+    }
+
+    @Test
+    void testComte() {
+        Item[] items = new Item[] { new Item("Comté", 10, 20) };
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals("Comté", items[0].name);
+        assertEquals(9, items[0].sellIn);
+        assertEquals(21, items[0].quality);
+    }
+
+    @Test
+    void testPassVip10Days(){
+        Item[] items = new Item[] { new Item("Pass VIP Concert", 10, 20) };
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals("Pass VIP Concert", items[0].name);
+        assertEquals(9, items[0].sellIn);
+        assertEquals(22, items[0].quality);
+    }
+
+    @Test
+    void testPassVip5Days(){
+        Item[] items = new Item[] { new Item("Pass VIP Concert", 5, 20) };
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals("Pass VIP Concert", items[0].name);
+        assertEquals(4, items[0].sellIn);
+        assertEquals(23, items[0].quality);
+    }
+
+    @Test
+    void testKryptonite(){
+        Item[] items = new Item[] { new Item("Kryptonite", 0, 80) };
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals("Kryptonite", items[0].name);
+        assertEquals(0, items[0].sellIn);
+        assertEquals(80, items[0].quality);
+    }
+
+    /*************************************************************************/
+
+    @Test
+    void testRegularItemQuality() {
+        Item[] items = new Item[] { new Item("Regular Item", 10, 0) };
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals(9, items[0].sellIn);
+        assertEquals(0, items[0].quality);
+    }
+
+    @Test
+    void testComteQuality() {
+        Item[] items = new Item[] { new Item("Comté", 10, 50) };
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals("Comté", items[0].name);
+        assertEquals(9, items[0].sellIn);
+        assertEquals(50, items[0].quality);
+    }
+
+    @Test
+    void testPassVipQuality(){
+        Item[] items = new Item[] { new Item("Pass VIP Concert", 1, 50) };
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals("Pass VIP Concert", items[0].name);
+        assertEquals(0, items[0].sellIn);
+        assertEquals(50, items[0].quality);
+    }
+
+    @Test
+    void testPassVipQualityDay0(){
+        Item[] items = new Item[] { new Item("Pass VIP Concert", 0, 50) };
+        Magasin app = new Magasin(items);
+        app.updateQuality();
+        assertEquals("Pass VIP Concert", items[0].name);
+        assertEquals(-1, items[0].sellIn);
+        assertEquals(0, items[0].quality);
+
     }
 
 }
